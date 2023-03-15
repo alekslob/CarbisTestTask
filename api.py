73c3f98b8c9964b41787ccecf3392dbe78695891
asalekslob <asalekslob@gmail.com>
2023-03-15 23:55:01 +0700

    api

--- api.py
+++ api.py
@@ -11,15 +11,7 @@ def get_coordinates(key:str, address:str, language:str)->list:
     result=result[0]
     return [result['data']['geo_lat'], result['data']['geo_lon']]
 
-if __name__ == '__main__':
-    token = input("token: ")
-    query = input("address: ")
-    print(get_coordinates(token, query, 'ru'))
-# token = input("token: ")
-# dadata = Dadata(token)
-# result = dadata.suggest("address", "москва хабар")
-# # print([r['value'] for r in result])
-# print([r['unrestricted_value'] for r in result])
-# result = dadata.suggest("address", result[5]['unrestricted_value'], count=1)
-# result=result[0]
-# print( result['data']['geo_lat'], result['data']['geo_lon'])
+# if __name__ == '__main__':
+#     token = input("token: ")
+#     query = input("address: ")
+#     print(get_coordinates(token, query, 'ru'))
