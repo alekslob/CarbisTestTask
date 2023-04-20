c212f04cedc9c9fb20dd566aa6e4c0cefacfbb6e
Слободчикова Александра <asalecslob@gmail.com>
2023-04-20 17:57:11 +0700

    new db

--- db.py
+++ db.py
@@ -4,17 +4,25 @@ class DBClient:
     def __init__(self, dbname:str="settings.db") -> None:
         self.connect = sqlite3.connect(dbname)
         self.cursor = self.connect.cursor()
+        self.__check_db()
     
-    def get_settings(self, settings):
+    def __check_db(self) -> None:
         response = self.cursor.execute("select name from sqlite_master")
-        if response.fetchone() is None: return settings
+        if response.fetchone() is None: self.__create_db()
         # таблицы не существует
-        
         self.cursor.execute("select count(id) from settings;")
         response = self.cursor.fetchall()
-        if response[0][0] == 0: return settings
+        if response[0][0] == 0: self.__create_lens()
         # записей не существует
 
+    def __create_db(self) -> None:
+        prepare_command = "create table if not exists settings (id int primary key, url varchar(255), key varchar(50), language varchar(2));"
+        self.cursor.execute(prepare_command)
+    def __create_lens(self) -> None:
+        self.save_settings({'url':'', 'key':'', 'language':''})
+
+    def get_settings(self):
+        settings = {}
         self.cursor.execute("select * from settings WHERE id=1;")
         response = self.cursor.fetchall()
         response = response[0]
@@ -27,14 +35,18 @@ class DBClient:
         url = settings.get("url")
         key = settings.get("key")
         language = settings.get("language")
-        prepare_command = "create table if not exists settings (id int primary key, url varchar(255), key varchar(50), language varchar(2));"
-        self.cursor.execute(prepare_command)
+        
         command = f"insert or replace into settings (id, url, key, language) values (1, '{url}', '{key}', '{language}');"
         self.cursor.execute(command)
         self.connect.commit()
 
-# if __name__ == '__main__':
-#     dbclient = DBClient()
-#     settings = {'url': 'https://dadata.ru/api/', 'key': '123', 'language': 'ru'}
-#     dbclient.save_settings(settings)
-#     print(dbclient.get_settings())
+if __name__ == '__main__':
+    dbclient = DBClient()
+    # settings = {'url': 'https://dadata.ru/api/', 'key': '123', 'language': 'ru'}
+    # dbclient.save_settings(settings)
+    settings = dbclient.get_settings()
+    print(settings)
+    if settings['url'] =='': settings['url'] = 'гег'
+    dbclient.save_settings(settings)
+    settings = dbclient.get_settings()
+    print(settings)
